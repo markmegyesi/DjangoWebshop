593b8802245dc055ae2dca67aa1a338cb7fbae4f
markmegyesi <aaylasecura17@gmail.com>
2022-12-09 14:39:33 +0100

    Add cart view implemented

--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -20,34 +20,29 @@ class CartView(ListView):
 
 @login_required
 def add_to_cart(request,product_id):
-    # if request.method == 'POST':
-    
     product = get_object_or_404(ProductModel, pk=product_id)
     cart_qs = CartModel.objects.filter(user=request.user)
     
     
     if cart_qs.exists():
         cart = cart_qs[0]
-        cart_item = CartItemModel.objects.get_or_create(product=product)
-        print(cart_item[0])
-        # cart_item =CartItemModel(product=product)
-        # cart_item.save()
-        # cart_item.cart.add(cart)
-        # cart_item.save()
-        print (cart_item)
-        if cart_item.product == product_id:
-            cart_item.quantity += 1
-            cart_item.save()
+        cart_item = CartItemModel.objects.filter(product=product)
+       
+        if cart_item.exists():
+            cart_item_model = CartItemModel.objects.get(product=product)
+            cart_item_model.quantity += 1
+            cart_item_model.save()
             messages.info(request,f"{product.product_name} has been added to your cart.")
             return(redirect('shop-home'))
             
-        # else:
-        #     cart_item.cart.add(cart_item)
-        #     messages.info(request, f"{product.product_name} has been added to your cart.")
-        #     return(redirect('shop-home'))
+        else:
+            cart_item_model= CartItemModel.objects.create(product=product)
+            cart_item_model.cart.add(cart)
+            messages.info(request, f"{product.product_name} has been added to your cart.")
+            return(redirect('shop-home'))
     else:
         cart = CartModel.objects.create(user=request.user)
-        cart_item_model= CartItemModel.objects.create(cart=cart)
+        cart_item_model= CartItemModel.objects.create(product=product)
         cart_item_model.cart.add(cart)
         messages.info(request, f"{product.product_name} has been added to your cart.")
         return(redirect('shop-home'))
