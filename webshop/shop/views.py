3af521c071e2ca4a0eceea2c2ba5d6876fe581c7
markmegyesi <aaylasecura17@gmail.com>
2022-12-12 21:33:01 +0100

    sum prices

--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -22,7 +22,17 @@ class CartView(ListView):
             user_id = self.request.user.id
             cart = CartModel.objects.get(user=user_id, ordered=False)
             data = CartItemModel.objects.filter(cart=cart.pk).select_related('product').all()
+            self.total_price=0
+            for i in data:
+                self.total_price+=i.total
             return data
+
+    def get_context_data(self, **kwargs):
+        
+        context = super(CartView, self).get_context_data(**kwargs)
+        context['total_price'] = self.total_price
+        return context
+    
 
 @login_required
 def add_to_cart(request):
@@ -39,7 +49,7 @@ def add_to_cart(request):
 
             if cart_item.exists():
                 cart_item_model = CartItemModel.objects.get(product=product)
-                cart_item_model.quantity += quantity
+                cart_item_model.quantity += int(quantity)
                 cart_item_model.save()
                 messages.info(request,f"{product.product_name} has been added to your cart.")
                 return(redirect('shop-home'))
@@ -116,4 +126,17 @@ class RecieptListView(ListView):
             user_id = self.request.user.id
             cart = CartModel.objects.latest('user')
             data = OrderModel.objects.filter(cart=cart).all()
+            self.total_gross_price=0
+            self.total_net_price=0
+            for i in data:
+                self.total_gross_price+=i.gross_price
+                self.total_net_price+=i.net_price
             return data
+            
+
+    def get_context_data(self, **kwargs):
+        
+        context = super(RecieptListView, self).get_context_data(**kwargs)
+        context['total_gross_price'] = self.total_gross_price
+        context['total_net_price'] = self.total_net_price
+        return context
